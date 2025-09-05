2636239a04257be7825de2e0ca04368d318f28c8
Leonardo <leopassos.leite@gmail.com>
2025-09-05 10:38:59 -0300

    :sparkles: bootstrap: solução TriggerMail (Clean-Lite com Service + Core, Swagger e health)

--- src/TriggerMail.Service/Program.cs
+++ src/TriggerMail.Service/Program.cs
@@ -1,13 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// TODO: registrar adapters (DbContext Sqlite, InMemoryQueue, SmtpEmailProvider, HandlebarsRenderer)
-// TODO: registrar EmailWorker (BackgroundService)
-
 var app = builder.Build();
-if (app.Environment.IsDevelopment()) { app.UseSwagger(); app.UseSwaggerUI(); }
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.MapGet("/health", () => Results.Ok(new { ok = true, service = "TriggerMail" }));
+
 app.MapControllers();
 app.Run();
